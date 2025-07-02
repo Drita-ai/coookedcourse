15c835a8e2697619da4607fce7f2f2cb8e50651b
alok <alok.kr300@gmail.com>
2025-07-02 19:33:12 +0530

    Improved positioning of labels and color of sidebar

--- client/src/components/LeftFloatingBox.jsx
+++ client/src/components/LeftFloatingBox.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { FaBahai, FaBacon } from "react-icons/fa6";
-import { FaHome } from 'react-icons/fa'
+import { FaHome } from 'react-icons/fa';
 
 const navItems = [
     { icon: <FaHome />, label: 'Home' },
@@ -19,40 +19,43 @@ function LeftFloatingBox() {
     return (
         <div
             className={`
-        fixed left-0 top-1/4
-        h-1/2 w-20
-        bg-black/60 backdrop-blur-lg border-r border-white/10
-        text-white rounded-r-3xl shadow-xl
-        flex flex-col items-center justify-around
-        transition-all duration-700 ease-out
-        ${loaded ? 'translate-x-0 opacity-100' : '-translate-x-10 opacity-0'}
-      `}
+            fixed left-0 top-1/4
+            h-1/2 w-20
+            bg-black/90 backdrop-blur-lg border-r border-white/10
+            text-white rounded-r-3xl shadow-xl
+            flex flex-col items-center justify-around
+            transition-all duration-700 ease-out
+            ${loaded ? 'translate-x-0 opacity-100' : '-translate-x-10 opacity-0'}
+        `}
         >
             {navItems.map((item, index) => (
                 <div
                     key={index}
-                    className="group relative flex items-center justify-center w-full"
+                    className="relative flex items-center justify-center w-full"
                     onClick={() => setActive(index)}
                 >
                     {/* Active indicator */}
                     <div className={`absolute left-0 h-10 w-1 rounded-r bg-cyan-400 transition-all ${active === index ? 'opacity-100' : 'opacity-0'}`}></div>
 
-                    <div
-                        className={`m-2 p-3 rounded-xl transition-all duration-300 cursor-pointer 
-              hover:bg-white/10 hover:shadow-md hover:scale-110 
-              ${active === index ? 'bg-white/10 scale-110' : ''}`}
-                    >
-                        {item.icon}
-                    </div>
+                    {/* Icon button becomes the hover group */}
+                    <div className="group relative">
+                        <div
+                            className={`m-2 p-3 rounded-xl transition-all duration-300 cursor-pointer 
+                                hover:bg-white/10 hover:shadow-md hover:scale-110 
+                                ${active === index ? 'bg-white/10 scale-110' : ''}`}
+                        >
+                            {item.icon}
+                        </div>
 
-                    {/* Tooltip */}
-                    <span className="absolute left-24 bg-black text-white text-sm px-2 py-1 rounded shadow-md opacity-0 group-hover:opacity-100 scale-90 group-hover:scale-100 transition-all duration-300 whitespace-nowrap">
-                        {item.label}
-                    </span>
+                        {/* Tooltip now only on icon hover */}
+                        <span className="absolute left-20 top-1/2 -translate-y-1/2 bg-black text-white text-sm px-2 py-1 rounded shadow-md opacity-0 group-hover:opacity-100 scale-90 group-hover:scale-100 transition-all duration-300 whitespace-nowrap z-10">
+                            {item.label}
+                        </span>
+                    </div>
                 </div>
             ))}
         </div>
     );
 }
 
-export default LeftFloatingBox
+export default LeftFloatingBox;
