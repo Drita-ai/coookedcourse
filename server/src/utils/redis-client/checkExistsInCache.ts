248b26ba1ac0f48e975024d02a38dbd6377875cc
Drita-ai <alok.kr300@gmail.com>
2025-07-26 12:47:02 +0530

    Added functionality to check cache-key

---
+++ server/src/utils/redis-client/checkExistsInCache.ts
@@ -0,0 +1,39 @@
+import { getRedisClient } from './getRedisClient'
+import axios from 'axios';
+
+import { CK_COOKED_PLAYLIST_LIST } from '../../constants/constants';
+
+export const checkInCacheAndSet = async (key: string, URL: string) => {
+    /**
+     * TODO: Better redis keys CHECK 
+     */
+    const client = await getRedisClient();
+
+    // Check the API Response in Cache
+    if (!(await client.get(key))) {
+        // Fetch the Playlist List
+        const playlistsItem = await axios.get(URL)
+
+        // Store it's result in cache
+        await client.set(CK_COOKED_PLAYLIST_LIST, JSON.stringify(playlistsItem!.data))
+    }
+}
+
+export const fetchTemplate = async (cacheKey: string, URL: string) => {
+    let playlistsItem;
+
+    try {
+        const client = await getRedisClient()
+
+        // Check the KEY in cache, if not exists SET the KEY and VALUE
+        await checkInCacheAndSet(cacheKey, URL)
+
+        // Get the PLAYLISTS LIST from cache
+        playlistsItem = JSON.parse((await client.get(cacheKey))!)
+    } catch (err) {
+        console.log(err)
+    }
+
+    return playlistsItem
+}
+
