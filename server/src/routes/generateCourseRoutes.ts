94df87053c7bf4001583c8d89b362d2f49fb6f15
Drita-ai <alok.kr300@gmail.com>
2025-07-31 18:12:15 +0530

    Added protect to generateCourse

--- server/src/routes/generateCourseRoutes.ts
+++ server/src/routes/generateCourseRoutes.ts
@@ -1,12 +1,13 @@
 import express from 'express'
 
 import generateCourseController from '../controllers/generateCourseController'
+import authController from '../controllers/authController'
 
 const router = express.Router();
 
 router
     .route('/generate-course')
-    .post(generateCourseController.generateCourse)
+    .post(authController.protect, generateCourseController.generateCourse)
 
 
 export { router }
