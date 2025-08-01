0a23214f333cebdab9b6afecd1e9f001b0d28432
Drita-ai <alok.kr300@gmail.com>
2025-08-01 09:57:00 +0530

    Added necessary constants

--- server/src/constants/constants.ts
+++ server/src/constants/constants.ts
@@ -1,1 +1,7 @@
 export const CK_COOKED_PLAYLIST_LIST = 'cookedplaylist-list'
+
+export const CK_COOKED_PLAYLIST_VIDEOS_LIST = 'cookedplaylist-videos-list'
+
+export const CK_CHANNELS_WITH_VIDEOS_TITLES = 'cookedchannels-videos-titles'
+
+export const CK_CHANNELS_WITH_VIDEOS_DETAILS = 'cookedchannels-videos-details'
