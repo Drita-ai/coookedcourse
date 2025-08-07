485a09a848880b8f0cacff5197a7dcaea3a23f82
Drita-ai <alok.kr300@gmail.com>
2025-08-07 11:59:04 +0530

    Removed bg blur from spinner

--- client/src/components/Spinner.jsx
+++ client/src/components/Spinner.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Spinner = () => {
     return (
-        <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-sm bg-black/30">
+        <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-none bg-black/30">
             <div className="w-16 h-16 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
         </div>
     );
