d386b23b538a82cb4979109c358d73da699573c7
Drita-ai <alok.kr300@gmail.com>
2025-08-08 13:19:49 +0530

    Configured cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,16 @@
 import express from 'express'
 import morgan from 'morgan'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 import { router as generateCookedCourseRouter } from './routes/generateCourseRoutes'
 
 const app = express()
 
 if (process.env.NODE_ENV = 'development') app.use(morgan('dev'))
+
+// CORS
+app.use(cors())
 
 // Essential Middlewares
 app.use(express.json())
