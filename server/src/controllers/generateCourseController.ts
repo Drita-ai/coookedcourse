8de7e6256b32f2b594c46640453ece117eefe4d1
Drita-ai <alok.kr300@gmail.com>
2025-08-28 10:17:21 +0530

    feat: Added saveToDB and revamp generateCourseController

--- server/src/controllers/generateCourseController.ts
+++ server/src/controllers/generateCourseController.ts
@@ -11,12 +11,43 @@ import prisma from '../utils/prisma-client/getPrismaClient'
 const AGENT_ROUTE = 'http://agent_container:8000/api/v1/agent'
 
 const generateCourse = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    console.log(req.__cc_final_syllabus, req.__cc_reference_ids)
+    let extractedTopics: CookedTopics = {};
+
     // 1) GET Topics of SYLLABUS from AI-AGENT
-    const extractedTopics: CookedTopics = (await axios.post(`${AGENT_ROUTE}/generate-topics`, {
-        syllabus: req.body
-    })).data
+    if (Object.keys(req.__cc_final_syllabus!).length !== 0) {
+        extractedTopics = (await axios.post(`${AGENT_ROUTE}/generate-topics`, {
+            syllabus: req.__cc_final_syllabus
+        })).data
+
+        // Save the extractedTopics in DB to serve in further queries 
+        await saveToDB(Object.values(extractedTopics), Object.values(req.__cc_final_syllabus!))
+    }
 
     const subject: string = req.body.subject;
+
+    // Get all extracted Units from reference ids
+    const storedUnitData = await prisma.unit.findMany({
+        where: { id: { in: req.__cc_reference_ids } },
+        select: { chapter: true }
+    })
+
+    let initialUnitCount = 0;
+    let extractedTopicsLength = Object.keys(extractedTopics).length;
+
+    if (extractedTopicsLength !== 0) {
+        const lastKey = Object.keys(extractedTopics)[extractedTopicsLength - 1];
+        initialUnitCount = Number(lastKey.charAt(lastKey.length - 1))
+    }
+
+    // Structure Unit data from DB along with extracted topics
+    extractedTopics = storedUnitData.reduce((acc, chapter, i) => {
+        const key = `Unit${i + 1 + initialUnitCount}`;
+
+        acc[key] = chapter.chapter;
+        return acc;
+    }, extractedTopics as Record<string, any>)
+
 
     // 2) Playlist Maker Instance
     const pm = new PlaylistMaker(extractedTopics, subject);
@@ -74,5 +105,38 @@ const checkInDB = catchAsync(async (req: Request, res: Response, next: NextFunct
 
     next();
 })
+
+const saveToDB = async (unitData: any, syllabus: string[]) => {
+    try {
+        for (let i = 0; i < unitData.length; i++) {
+            // Create Unit
+            const unit = await prisma.unit.create({
+                data: {},
+            });
+
+            // Create Syllabus
+            await prisma.syllabus.create({
+                data: {
+                    syllabus: syllabus[i],
+                    unitId: unit.id,
+                },
+            });
+
+            // Create chapters
+            const chapters = unitData[i];
+            for (const chapter of chapters) {
+                await prisma.chapter.create({
+                    data: {
+                        name: chapter.name,
+                        topics: chapter.topics,
+                        unitId: unit.id,
+                    },
+                });
+            }
+        }
+    } catch (err) {
+        console.error(err)
+    }
+}
 
 export default { generateCourse, checkInDB }
